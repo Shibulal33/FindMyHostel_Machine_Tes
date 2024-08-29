900ba0e89ae7bd61b1fe5f00d5d414e7d710a3ac
Shibulal33 <shibulal33@gmail.com>
2024-08-29 22:03:17 +0530

    production url added

--- Frontend/src/pages/config/config.jsx
+++ Frontend/src/pages/config/config.jsx
@@ -1,7 +1,7 @@
 let BASE_URL = null
-const production = false
+const production = true
 if (production) {
-    BASE_URL = "http://3.107.90.239:7000"
+    BASE_URL = "http://16.171.24.157:4000"
 } else {
     BASE_URL = "http://localhost:4000"
 }
