b0a9c80be74fcb92cec5c38bab44667657d5b93b
Shibulal33 <shibulal33@gmail.com>
2024-08-29 20:49:02 +0530

    bill model - bill type default value changed

--- Backend/src/models/billModel.js
+++ Backend/src/models/billModel.js
@@ -11,7 +11,7 @@ const billSchema = new mongoose.Schema({
     },
     billType: {
         type: String,
-        enum: ["security", "others"],
+        enum: ["security", "other"],
         default: "security"
     },
     strActiveStatus: { type: String },
